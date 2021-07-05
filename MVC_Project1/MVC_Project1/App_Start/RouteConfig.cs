d92dca89dd93e8aa4016df2d4a5a196d868354bf
Nguyenvantay <43233472+17110220@users.noreply.github.com>
2021-07-05 12:22:17 +0700

    add login page

--- MVC_Project1/MVC_Project1/App_Start/RouteConfig.cs
+++ MVC_Project1/MVC_Project1/App_Start/RouteConfig.cs
@@ -16,8 +16,9 @@ namespace MVC_Project1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Dammio", action = "Index", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
