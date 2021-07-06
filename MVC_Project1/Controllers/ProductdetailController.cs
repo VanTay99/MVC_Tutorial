cad0433cec6cbaa71b7ff47e3cb56824d5c6907a
Nguyenvantay <43233472+17110220@users.noreply.github.com>
2021-07-06 14:53:08 +0700

    Add-fake-database

--- MVC_Project1/Controllers/ProductdetailController.cs
+++ MVC_Project1/Controllers/ProductdetailController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVC_Project1.Models;
 namespace MVC_Project1.Controllers
 {
     public class ProductdetailController : Controller
     {
         // GET: Productdetail
-        public ActionResult Index()
+        public ActionResult Productdetail()
         {
-            return View();
+            var Iphonex = new Product() { name = "IPhone X", lname ="iphone-x",Price=17000000,RatingValue=100,img= "~/Content/Images/iphone-12-pro-max.jpg" };
+            return View(Iphonex);
         }
     }
 }
