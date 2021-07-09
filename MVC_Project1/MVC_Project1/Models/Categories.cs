ed4ca2ca82d3ecb0a9fb28caefb571bb7558c44b
Nguyenvantay <43233472+17110220@users.noreply.github.com>
2021-07-09 11:00:01 +0700

    update

---
+++ MVC_Project1/MVC_Project1/Models/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project1.Models
+{
+    public class Categories
+    {
+        public string Id { get; set; }
+        public string name { get; set; }
+    }
+}
