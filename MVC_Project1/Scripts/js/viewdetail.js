86d7502c46cba035cccb7c3b372a522185ba70d3
Nguyenvantay <43233472+17110220@users.noreply.github.com>
2021-07-13 16:30:08 +0700

    fix css

---
+++ MVC_Project1/Scripts/js/viewdetail.js
@@ -0,0 +1,17 @@
+﻿let result = document.getElementById('result');
+var val
+function handleChange() {
+    result = document.getElementById('result');
+}
+function giam() {
+    if (result.value > 1) {
+        val = Number(result.value) - 1;
+    }
+    else val = 1;
+    result.setAttribute('value',val)
+}
+function tang() {
+    val = Number(result.value) + 1;
+    result.setAttribute('value', val);
+    
+}
