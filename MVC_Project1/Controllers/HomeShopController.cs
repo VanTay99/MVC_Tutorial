baf017bd9f2aa769b31eeb889cf6bc729f9e8639
Nguyenvantay <43233472+17110220@users.noreply.github.com>
2021-07-19 16:00:10 +0700

    demoAjax.js

--- MVC_Project1/Controllers/HomeShopController.cs
+++ MVC_Project1/Controllers/HomeShopController.cs
@@ -15,8 +15,6 @@ namespace MVC_Project1.Controllers
             return View();
         }
         
-       
-
         public ActionResult ViewProduct()
         {
             var products = GetListProducts();
@@ -38,7 +36,7 @@ namespace MVC_Project1.Controllers
                 {
                     Id="1",
                     brand="Apple",
-                    name="Iphone 12 Pro Max ",
+                    name="Iphone 12 Pro Max",
                     url="/Content/desktop/images/iphone12_1.jpg",
                     Price=28950,
                     Description="Máy mới chính hãng Apple Việt Nam",
--- MVC_Project1/Scripts/js/demoAjax.js
+++ MVC_Project1/Scripts/js/demoAjax.js
@@ -1,14 +1,17 @@
-﻿function loadDoc() {
+﻿
+// demo Ajax voi file text
+function loadDoc() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML =
-                this.responseText;
+            document.getElementById("demo").innerHTML = this.responseText;
         }
-    }
-    xhttp.open("GET", "Content/txt/TextFile1.txt", true);
+    };
+    xhttp.open("GET", "/Content/txt/ajax_info.txt", true);
     xhttp.send();
+
 }
+// demo Ajax voi file xml
 function loadXML() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
@@ -20,6 +23,45 @@ function loadXML() {
         }
         document.getElementById("demo").innerHTML = txt;
     }
-    xhttp.open("GET", "Content/txt/XMLFile1.xml", true);
+    xhttp.open("GET", "/Content/xml/ajax_info.xml");
     xhttp.send();
 }
+// demo Ajax voi file asp
+function loadASP(str) {
+    var xhttp;
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/Content/asp/gethint.asp?q=" + str, true);
+    xhttp.send();
+}
+//#region demo Ajax Jquery
+//Ajax khong dong bo
+$(document).ready(function () {
+    $("#button1").click(function () {
+        $.ajaxSetup({
+            url: "/Content/txt/ajax_info.txt", success: function (result) {
+                $("#div1").html(result);
+            }
+        });
+        $.ajax();
+    });
+});
+
+$(document).ready(function () {
+    $("#button2").click(function () {
+        $.get("/Content/asp/gethint.asp",function (data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
+
+
+//#endregion 
